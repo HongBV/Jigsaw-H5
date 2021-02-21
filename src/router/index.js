619460333d25d0804a6f183195d6bb8871bbdd57
hongruiqi <939199534@qq.com>
2021-02-22 00:17:23 +0800

    fix: 修复路由跳转异常错误

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
+// 处理路由跳转异常错误
+const originalPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location, onResolve, onReject) {
+  if (onResolve || onReject)
+    return originalPush.call(this, location, onResolve, onReject);
+  return originalPush.call(this, location).catch(err => err);
+};
+
 Vue.use(VueRouter);
 
 const routes = [
