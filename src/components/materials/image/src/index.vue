420f3f6e7f0208d81e75ec1e3088ec174e3ebe83
hongruiqi <939199534@qq.com>
2021-02-24 01:53:45 +0800

    feat: 修改图片组件的默认图片

--- src/components/materials/image/src/index.vue
+++ src/components/materials/image/src/index.vue
@@ -15,8 +15,7 @@ export default {
     config: {
       type: Object,
       default: () => ({
-        url:
-          "https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg",
+        url: "https://img01.yzcdn.cn/vant/tree.jpg",
         fit: "cover",
         borderRadius: 0
       })
--- src/config/material-config.js
+++ src/config/material-config.js
@@ -164,8 +164,7 @@ const materialsList = [
       }
     ],
     config: {
-      url:
-        "https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg",
+      url: "https://img01.yzcdn.cn/vant/tree.jpg",
       fit: "cover",
       borderRadius: 0
     }
