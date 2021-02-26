a8ea3f250c73451e0cb16bb94c69a0c103d23d83
hongruiqi <939199534@qq.com>
2021-02-26 15:55:15 +0800

    feat: 优化配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,11 @@
+const localhost = "192.168.1.100";
 module.exports = {
   devServer: {
-    host: "192.168.1.101", //target host
+    host: localhost, //target host
     port: 8080,
     proxy: {
       "^/api": {
-        target: "http://192.168.1.101:7001", //代理地址，这里设置的地址会代替axios中设置的baseURL
+        target: `http://${localhost}:7001`, //代理地址，这里设置的地址会代替axios中设置的baseURL
         changeOrigin: true // 如果接口跨域，需要进行这个参数配置
       }
     }
