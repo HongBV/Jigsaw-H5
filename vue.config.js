cd57f774358d8f31171e917c47bad5a8a8dfa0ba
hongruiqi <939199534@qq.com>
2021-03-11 16:43:51 +0800

    feat: gzip

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
 module.exports = {
   devServer: {
     port: 8080
+  },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === "production") {
+      config.plugins.push(
+        new CompressionWebpackPlugin({
+          test: /\.js$|\.html$|\.css$/,
+          threshold: 4096
+        })
+      );
+    }
   }
 };
