18ea55b85dbc3b5d78db4a215fa341af951152c1
hongruiqi <939199534@qq.com>
2021-05-09 13:05:33 +0800

    feat: 本地调试配置

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 // 每次请求携带cookies信息
 // axios.defaults.withCredentials = true;
-// 创建axios实例
 const service = axios.create({
-  baseURL: "http://106.14.90.39", // api的base_url
+  // baseURL: "http://106.14.90.39",
+  baseURL: "http://localhost:7001",
   timeout: 5000 // 请求超时时间
 });
 export default service;
