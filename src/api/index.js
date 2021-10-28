3999994071bf9fb396233a658b310b3d5ec35a9a
hongruiqi <939199534@qq.com>
2021-10-28 21:38:23 +0800

    feat: 修改baseurl

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 // 每次请求携带cookies信息
-// axios.defaults.withCredentials = true;
 const service = axios.create({
-  // baseURL: "http://106.14.90.39",
-  baseURL: "http://localhost:7001",
+  baseURL: "http://106.14.90.39:7001",
   timeout: 5000 // 请求超时时间
 });
 export default service;
